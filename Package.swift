759296f0b4ea1483c86d8db1839da9eec87b793d
Tim Brooks <tim.a.brooks@disney.com>
2022-06-17 23:21:45 -0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -6,23 +6,21 @@ import PackageDescription
 let package = Package(
     name: "RustyPlugins",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "RustyPlugins",
-            targets: ["RustyPlugins"]),
+        .plugin(
+            name: "ShowTodosPlugin",
+            targets: [
+                "ShowTodos"
+            ]
+        )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RustyPlugins",
-            dependencies: []),
-        .testTarget(
-            name: "RustyPluginsTests",
-            dependencies: ["RustyPlugins"]),
+        .plugin(name: "ShowTodos",
+                capability: .command(
+                    intent: .custom(
+                        verb: "show-todos",
+                        description: "Displays Todos and FixMes in the Project"),
+                permissions: [])
+        )
     ]
 )
---
+++ Plugins/ShowTodos/Plugin.swift
@@ -0,0 +1,17 @@
+//
+//  Plugin.swift
+//  
+//
+//  Created by Tim Brooks on 6/17/22.
+//
+
+import Foundation
+import PackagePlugin
+
+@main
+struct ShowTodos: CommandPlugin {
+
+    func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
+        
+    }
+}
