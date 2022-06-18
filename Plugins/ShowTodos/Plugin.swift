18078c3667a996f03c8ef9f86f3e29c8b2d273d9
Tim Brooks <tim.a.brooks@disney.com>
2022-06-18 09:36:46 -0700

    source file processing

--- Plugins/ShowTodos/Plugin.swift
+++ Plugins/ShowTodos/Plugin.swift
@@ -12,16 +12,20 @@ import PackagePlugin
 struct ShowTodos: CommandPlugin {
 
     func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
-        
+
         for target in context.package.targets {
             
             guard let target = target as? SourceModuleTarget else { continue }
             let sourceFileList = target.sourceFiles(withSuffix: ".swift")
             let sourceFiles = sourceFileList.map(\.path)
             
-            for file in sourceFiles {
-                Diagnostics.remark("Processing: \(file.lastComponent)")
-            }
+            processSourceFiles(sourceFiles)
+        }
+    }
+    
+    private func processSourceFiles(_ paths: [Path]) {
+        for path in paths {
+            Diagnostics.remark("Processing: \(path.lastComponent)")
         }
     }
 }
@@ -34,6 +38,12 @@ extension ShowTodos: XcodeCommandPlugin {
     /// 👇 This entry point is called when operating on an Xcode project.
     func performCommand(context: XcodePluginContext, arguments: [String]) throws {
        print("Command plugin execution for Xcode project \(context.xcodeProject.displayName)")
+        
+        for target in context.xcodeProject.targets {
+
+            let sourceFiles = target.inputFiles.filter({ $0.path.lastComponent.contains(".swift") }).map(\.path)
+            processSourceFiles(sourceFiles)
+        }
     }
 }
 #endif
